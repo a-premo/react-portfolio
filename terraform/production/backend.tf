b8f56350fb2aa6f4146dede09e19ccc816b9b031
a-premo <alex@eastontech.net>
2024-02-08 11:13:39 -0500

    Remove old files

--- terraform/production/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "alex-premo-terraform-state"
-    key            = "alex_premo_prod/alex-premo-terraform-state.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "alexpremoProdDB"
-  }
-}
